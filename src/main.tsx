1085979fe4268edd76c41f4fa6394b47fef6c812
cballevre <celestin.ballevre@protonmail.com>
2024-07-07 16:56:36 +0200

    feat: Add router and remove boilerplate

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,19 @@
 import React, { useState } from "react";
-import "./main.css";
-import reactLogo from "./assets/react.svg";
-import FarmLogo from "./assets/logo.png";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 export function Main() {
-  const [count, setCount] = useState(0);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
 
   return (
-    <>
-      <div>
-        <a href="https://farmfe.org/" target="_blank">
-          <img src={FarmLogo} className="logo" alt="Farm logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Farm + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/main.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Farm and React logos to learn more
-      </p>
-    </>
+      <RouterProvider router={router} />
   );
 }

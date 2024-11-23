1585258786360bdd588edf27b6f3e671757adaed
cballevre <celestin.ballevre@protonmail.com>
2024-11-23 18:54:01 +0100

    feat: Add assumption page

---
+++ src/features/assumptions/components/AssumptionTable.tsx
@@ -0,0 +1,17 @@
+const AssumptionTable = () => {
+  return (
+    <div>
+      <h2>Assumptions (10)</h2>
+      <button>Add an assumption</button>
+      <ul>
+        <li>Impôt : -20€ - Septembre</li>
+        <li>Loyer : -600€ - Tous les mois</li>
+        <li>Assurance : -100€ - Tous les mois</li>
+        <li>Electricité : -50€ - Tous les mois</li>
+        <li>Salaire : 1600€ - Tous les mois</li>
+      </ul>
+    </div>
+  )
+}
+
+export { AssumptionTable }
